fd6a4c021b42f8ea6b916642046ad9ff78bcdb22
ayzcrem <angelajuarez@nyu.edu>
2023-05-02 21:55:58 -0400

    add env

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.static(publicURL));
 
 // Set your static html file
 app.get("/", (req, res) => {
-  res.sendFile( path.resolve(__dirname + "/views/index.html"))
+  res.sendFile( path.resolve(__dirname + "xx4everfift33nxx.html"))
 });
 
 // ---- ADD YOUR API ENDPOINTS HERE ----
